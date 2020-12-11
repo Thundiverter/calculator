4a0188ead6d5171fd4750a9048e8810892484169
Ivan Stickmanov <stickman9000@mail.ru>
2020-12-11 21:19:24 +0300

    First commit

---
+++ old/README.md
@@ -0,0 +1,8 @@
+### [Open in browser](https://jscalcapp.netlify.app/)
+My attempt to create a calculator that looks and works close to usual calculators.
+
+#### Current problems
+
+* "." button doesn't work yet.
+* Problems with "0" and "-0".
+* Accidental bugs.
---
+++ src/calculators/calc.js
@@ -0,0 +1,18 @@
+// Calculator
+
+console.log('works');
+
+let n1; // Number 1
+let a; // Action (+ - * /)
+let n2; // Number 2
+
+function update() {
+	if (n1 == false) {
+		
+	}
+}
+
+function act(action) {
+	if action.isNaN == false:
+		console.log(action);
+}
